8585a0c1436e6e51d8b4ad078bd3b1f71fa7c847
Cody Perry <perrysdevelopment@gmail.com>
2022-05-01 17:36:34 -0400

    Add useState, useEffect hooks and image files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import {useEffect, useState} from 'react'
 import './App.css';
 
+/**
+  *  slip: {
+  *     id: 75, 
+  *     advice: 'You will always regret the round of Tequila.'
+  *  }
+  * 
+ */
+
 function App() {
+  // Need state to store the required objects information
+  const [advice, setAdvice] = useState({slip: {id: "", advice: ""}})
+  
+  // Make API fetch request
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/advice')
+      .then(res => res.json())
+      .then(data => setAdvice(data))
+  }, [])
+
   return (
     <div className="App container">
       <div className="advice-box">
-        <h1>{/* Placeholder Icon */}</h1>
-        <p>{/* This will update once we set up useeffect */}</p>
+        {advice.slip.id && <h1>ADVICE #{advice.slip.id}</h1>}
+        {/* <h1>ADVICE #</h1> */}
+        <p>{}</p>
         <div>{/* Placeholder Icon */}</div>
         <div>{/* Placeholder Dice Image */}</div>
       </div>
