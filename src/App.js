f24c1ca83e123f7f06d48c1a7ff165958ee650a9
Cody Perry <perrysdevelopment@gmail.com>
2022-05-05 09:41:15 -0400

    Implement button click to get a new piece of advice from the API

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import divider from './images/pattern-divider-desktop.svg'
 import diceIcon from './images/icon-dice.svg'
 import './App.css';
 
-/** 
- * 1. Setup default text in markup sections
- * 2. Add a conditional that shows the default false and the incoming data from the API when true
- * 3. Style the advice app
-*/
-
 function App() {
   const [advice, setAdvice] = useState({slip: {id: "", advice: ""}})
   
-  // Make API fetch request
+  // Set the state upon load
   useEffect(() => {
     fetch('https://api.adviceslip.com/advice')
       .then(res => res.json())
       .then(data => setAdvice(data))
   }, [])
+
+  // Get a new piece of advice upon each button click
+  const handleClick = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then(res => res.json())
+      .then(data => setAdvice(data))
+  }
 
   return (
     <div className="container">
@@ -25,7 +26,9 @@ function App() {
         <h1>{advice.slip.id && `ADVICE #${advice.slip.id}`}</h1>
         <p>"{advice.slip.advice && advice.slip.advice}"</p>
         <img className="divider" src={divider} alt="Pattern Divider" />
-        <div className="dice-container">
+        <div 
+          className="dice-container"
+          onClick={handleClick}>
           <img className="dice-icon" src={diceIcon} alt="Dice Icon"/>
         </div>
       </div>
